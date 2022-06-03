16081dd1a10dc012fd37e46a5f72cc2edfa7143f
stooooopu <jyg5193@naver.com>
2022-06-03 11:03:51 +0900

    fix 0603ip수업

--- src/main/java/com/dw/board/interceptor/Interceptor.java
+++ src/main/java/com/dw/board/interceptor/Interceptor.java
@@ -1,11 +1,19 @@
 package com.dw.board.interceptor;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.dw.board.service.LogsService;
+import com.dw.board.vo.LogsVO;
 
 // Bean : 스프링이 관리하는 클래스
 // spring에서 제공하고 spring에서 관리하는 class : @Bean
@@ -13,6 +21,8 @@ import org.springframework.web.servlet.ModelAndView;
 @Component
 public class Interceptor implements HandlerInterceptor{
 
+	@Autowired
+	private LogsService logsService;
 	// ** controller에 도착전에 intercept하는 함수
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
@@ -20,10 +30,29 @@ public class Interceptor implements HandlerInterceptor{
 		
 		String url = request.getRequestURI();
 		String ip = request.getHeader("X-forwarded-For");
+		String httpMethod = request.getMethod();
 		if(ip == null) ip = request.getRemoteAddr(); 
 
 		System.out.println("IP ====> "+ip);
+		// IP ====> 0:0:0:0:0:0:0:1 : localHost
 		System.out.println("요청받은 URL은 ==> "+url);
+		System.out.println("HTTP Method ==> "+httpMethod);
+		
+		
+		SimpleDateFormat formatter = 
+				new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.KOREA);
+		String time = formatter.format(Calendar.getInstance().getTime());
+		System.out.println("time ==> "+time);
+		
+		LogsVO vo = new LogsVO();
+		vo.setUrl(url);
+		vo.setIp(ip);
+		vo.setHttpMethod(httpMethod);
+		vo.setLatitude("36.3286904");
+		vo.setLongitude("127.4229992");
+		vo.setCreateAt(time);
+		
+		logsService.setLogs(vo);
 		
 		return true;
 	}

b6d213119f2fc96f14d99289fb8358a696e8f645
stooooopu <jyg5193@naver.com>
2022-06-16 15:42:24 +0900

    fix logger추가 & board login으로 넘어가는 500에러 해결을 위해 return false추가

--- src/main/java/com/dw/board/interceptor/Interceptor.java
+++ src/main/java/com/dw/board/interceptor/Interceptor.java
@@ -8,6 +8,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -22,6 +25,9 @@ import com.dw.board.vo.LogsVO;
 @Component
 public class Interceptor implements HandlerInterceptor{
 
+	// syso이 아닌 실무에서 확인하는 방법
+	private static final Logger logger = LoggerFactory.getLogger(Interceptor.class);
+	
 	@Autowired
 	private LogsService logsService;
 	// ** controller에 도착전에 intercept하는 함수
@@ -34,16 +40,21 @@ public class Interceptor implements HandlerInterceptor{
 		String httpMethod = request.getMethod();
 		if(ip == null) ip = request.getRemoteAddr(); 
 
-		System.out.println("IP ====> "+ip);
-		// IP ====> 0:0:0:0:0:0:0:1 : localHost
-		System.out.println("요청받은 URL은 ==> "+url);
-		System.out.println("HTTP Method ==> "+httpMethod);
+		logger.info("Hello World");
+		logger.info("client IP : "+ip);
+		logger.info("request URL : "+url);
+		logger.info("request HTTPMethod : "+httpMethod);
+		
+//		System.out.println("IP ====> "+ip);
+//		// IP ====> 0:0:0:0:0:0:0:1 : localHost
+//		System.out.println("요청받은 URL은 ==> "+url);
+//		System.out.println("HTTP Method ==> "+httpMethod);
 		
 		
 		SimpleDateFormat formatter = 
 				new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.KOREA);
 		String time = formatter.format(Calendar.getInstance().getTime());
-		System.out.println("time ==> "+time);
+//		System.out.println("time ==> "+time);
 		
 		LogsVO vo = new LogsVO();
 		vo.setUrl(url);
@@ -56,17 +67,18 @@ public class Interceptor implements HandlerInterceptor{
 		logsService.setLogs(vo);
 		
 		
-		// 모든controller, restController에 세션을 가져와서 확인할 수 없으니 interceptor에 추가한것
+		// 모든controller, restController에 세션을 가져와서 확인할 수 없으니 Interceptor에 추가한것
 		// session 체크
 		HttpSession session = request.getSession();
-		if(session.getAttribute("studentsId") != null) {
-			int studentsId = (int)session.getAttribute("studentsId");
-			String studentsName = (String)session.getAttribute("studentsName");
-			System.out.println("session에서 가져온 id ==> "+studentsId);
-			System.out.println("session에서 가져온 name ==> "+studentsName);
-		}
+//		if(session.getAttribute("studentsId") != null) {
+//			int studentsId = (int)session.getAttribute("studentsId");
+//			String studentsName = (String)session.getAttribute("studentsName");
+//			System.out.println("session에서 가져온 id ==> "+studentsId);
+//			System.out.println("session에서 가져온 name ==> "+studentsName);
+//		}
 		if(session.getAttribute("studentsId") == null) {
 			response.sendRedirect("/login"); // session값이 없으면 "/login"경로로 redirect
+			return false; // 원래는 redirect로 바로 넘어야야하는데 return true로 돼서 false추가
 		}
 		
 		return true;
